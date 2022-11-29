588fcb3353c3a8d12d2727882348cd6596065a6a
Nickolodeon98 <sjeon0730@gmail.com>
2022-11-30 01:48:16 +0900

    feat: 새 프로젝트 생성

---
+++ src/main/java/com/example/springbootgitlabproject/SpringBootGitlabProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootgitlabproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGitlabProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootGitlabProjectApplication.class, args);
+    }
+
+}
