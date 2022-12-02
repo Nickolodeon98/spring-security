49792d57c375f33aed4b8dc38572a100f3cbe3f7
Nickolodeon98 <sjeon0730@gmail.com>
2022-12-02 11:09:06 +0900

    feat: 컨트롤러 테스트와 Swagger 추가

---
+++ src/test/java/com/example/springbootgitlabproject/controller/TeacherControllerTest.java
@@ -0,0 +1,60 @@
+package com.example.springbootgitlabproject.controller;
+
+import com.example.springbootgitlabproject.domain.dto.TeacherResponseDto;
+import com.example.springbootgitlabproject.service.TeacherService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(TeacherController.class)
+class TeacherControllerTest {
+
+    @MockBean
+    TeacherService teacherService;
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+
+    @Test
+    @DisplayName("성공 응답을 올바르게 한다.")
+    void success_test() throws Exception {
+
+        String name = "전승환";
+        int age = 25;
+
+        TeacherResponseDto teacherResponseDto = TeacherResponseDto.builder()
+                .name("전승환").age(25).build();
+
+        given(teacherService.addTeacherInfo(name, age)).willReturn(teacherResponseDto);
+
+        String url = "/api/v1/teachers/test";
+        mockMvc.perform(post(url).contentType(MediaType.APPLICATION_JSON)
+                .param(name).param(String.valueOf(age)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.resultCode").value("SUCCESS"))
+                .andExpect(jsonPath("$.resultCode.name").value("전승환"))
+                .andExpect(jsonPath("$.resultCode.age").value(25))
+                .andDo(print());
+
+        verify(teacherService).addTeacherInfo(name, age);
+    }
+
+}
