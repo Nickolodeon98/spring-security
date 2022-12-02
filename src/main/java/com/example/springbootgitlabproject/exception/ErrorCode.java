40e0e3afe51f03c0aad12ca0fd8ceb5d835066b0
Nickolodeon98 <sjeon0730@gmail.com>
2022-12-02 11:58:05 +0900

    feat: 아이디, 비밀번호 틀렸을 때 ErrorCode 추가

--- src/main/java/com/example/springbootgitlabproject/exception/ErrorCode.java
+++ src/main/java/com/example/springbootgitlabproject/exception/ErrorCode.java
@@ -3,7 +3,9 @@ package com.example.springbootgitlabproject.exception;
 import org.springframework.http.HttpStatus;
 
 public enum ErrorCode {
-    DUPLICATE_ERROR(HttpStatus.BAD_REQUEST, "빈 값이 아닙니다.");
+    DUPLICATE_ERROR(HttpStatus.BAD_REQUEST, "빈 값이 아닙니다."),
+    NOT_FOUND_ERROR(HttpStatus.NOT_FOUND, "아이디가 없습니다."),
+    PASSWORD_ERROR(HttpStatus.BAD_REQUEST, "비밀번호가 틀렸습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
