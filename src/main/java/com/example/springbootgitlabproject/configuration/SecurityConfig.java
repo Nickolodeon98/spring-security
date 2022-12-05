9a4da840e848e3775fdc524eba466d4eb25ca356
Nickolodeon98 <sjeon0730@gmail.com>
2022-12-05 15:35:54 +0900

    feat: SecurityConfig 클래스에 서비스와 키를 멤버 변수로 추가

--- src/main/java/com/example/springbootgitlabproject/configuration/SecurityConfig.java
+++ src/main/java/com/example/springbootgitlabproject/configuration/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.example.springbootgitlabproject.configuration;
 
+import com.example.springbootgitlabproject.service.TeacherService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -7,10 +10,16 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final TeacherService teacherService;
+    @Value("${jwt.token.secret}")
+    private String secretKey;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -25,7 +34,8 @@ public class SecurityConfig {
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // jwt사용하는 경우 씀
                 .and()
-//                .addFilterBefore(new JwtTokenFilter(userService, secretKey), UsernamePasswordAuthenticationFilter.class) //UserNamePasswordAuthenticationFilter적용하기 전에 JWTTokenFilter를 적용 하라는 뜻 입니다.
+                //UserNamePasswordAuthenticationFilter 적용하기 전에 JWTTokenFilter 를 적용 하라는 뜻 입니다.
+                .addFilterBefore(new JwtTokenFilter(teacherService, secretKey), UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 }
